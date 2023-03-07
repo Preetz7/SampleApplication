d17b9fa853660e731b9c4b42e9c0d2921e1e0283
Preetz7 <123237337+Preetz7@users.noreply.github.com>
2023-03-07 21:29:30 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Preetha!"); 
+        System.out.println("Hello, Preetha!");
+        System.out.println("Welcome to jenkins demo");
     }
 }
