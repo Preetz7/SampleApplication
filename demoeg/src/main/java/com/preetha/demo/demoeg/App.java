4c4fe21c178a15d821fa20b8f1ef3618b31dcf38
Preetz7 <preetzz079@gmail.com>
2023-03-18 22:27:11 +0530

    First commit

---
+++ demoeg/src/main/java/com/preetha/demo/demoeg/App.java
@@ -0,0 +1,13 @@
+package com.preetha.demo.demoeg;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
